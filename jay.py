4ea65ebc4811ed8689364490a6dce0a61f595ac9
jayanthreddy99 <jayanthreddy7371@gmail.com>
2023-08-14 14:13:53 +0530

    first commit

---
+++ jay.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template
+
+FAI = Flask(__name__)
+
+@FAI.route('/wish')
+def wish():
+    return 'hai how are you'
+
+@FAI.route('/first')
+def first():
+    return render_template('first.html',name = 'jayanthreddy',age='21')
+
+if __name__ == '__main__':
+    FAI.run(debug=True)
